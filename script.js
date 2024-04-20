17d67d5574fc4e16c6afca176e2c458303e525f8
Maik <noungouamaik2023@outlook.com>
2024-04-21 00:15:27 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+function addNumber (numbers) {
+    for (i = 0; i < numbers.length; i++) {
+        let total = 0; 
+        total =  
+        console.log(sum)
+    }
+    return sum 
+}
+
+
+
+addNumber([1,2,3,4,5,6,7,8])
+
+
+/*const mainDiv = document.getElementById("main");
+mainDiv.setAttribute("display", "grid");
+mainDiv.setAttribute("grid-templates-columns", "4");
+mainDiv.setAttribute("grid-templates-row", "5");
+
+
+
+//mainDiv.className = "red";*/
